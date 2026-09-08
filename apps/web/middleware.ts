bfc2d1b4a0906f74dcdc3b8482a567d1abaafb7a
Richard Nuhu <168305616+QweciKuranchie@users.noreply.github.com>
2026-09-08 15:18:32 +0000

    feat(auth): implement app-level middlewares and Clerk satellite route protection

---
+++ apps/web/middleware.ts
@@ -0,0 +1,10 @@
+import { createMiddleware } from "@repo/auth";
+
+export default createMiddleware(["/user(.*)", "/checkout(.*)"]);
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
